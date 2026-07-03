21081588c074d598839affbe305fd70b313f97c0
JB130-sigma <jeetcbhandari@gmail.com>
2026-07-03 20:50:21 +0530

    Update AdminSidebar.tsx

--- components/AdminSidebar.tsx
+++ components/AdminSidebar.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { useState } from "react";
-
 const navItems = [
   { label: "Main",       items: [
     { href: "/admin/dashboard",  icon: "📊", label: "Dashboard" },
